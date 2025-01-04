9aeb59f73b0b84247b7fd591624b609e2e00dc60
tomiwashelby <tomiwatolugbemi@gmail.com>
2025-01-05 00:15:11 +0100

    new changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
 	return (
-		<div className='mt-8 py-2 md:py-4 md:px-4 bg-teal-950 inter cursor-default'>
+		<div className='mt-8 py-2 md:py-4 md:px-4 bg-teal-950 inter cursor-default text-xs md:text-base'>
 			<h1 className='text-center mb-1'>Developed by <Link href={'/tomiwatolugbemi@gmail.com'} className='cursor-pointer'>TOMIWA TOLUGBEMI</Link></h1>
 			<span className='flex items-center justify-center raleway'><FaCopyright className='mr-2' />2024</span>
 		</div>
