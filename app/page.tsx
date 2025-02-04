e0bf02078316ab75b0cacab01b088595e8a25d7f
tomiwashelby <tomiwatolugbemi@gmail.com>
2025-02-04 14:49:33 +0100

    new changes

--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,7 @@ interface ApiResponse {
 export default function Home() {
   const [coins, setCoins] = useState<Coins[]>([]);
   const [loading, setLoading] = useState(true);
-  let ws = new WebSocket('');
-  const url = 'https://api.pro.coinbase.com';
+  const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
   const getCoins = async () => {
     setLoading(true);
@@ -31,7 +30,7 @@ export default function Home() {
       const res = await fetch('https://openapiv1.coinstats.app/coins?limit=1000&cache_bust=' + new Date().getTime(), {
         method: 'GET',
         headers: {
-          'X-API-KEY': 'AvUQcxPoOSa0Xvn9yuzHEpFcMqMin8QuvgMjptJkRHI=',
+          'X-API-KEY': `${API_KEY}`,
           'Accept': 'application/json'
         }
       });
@@ -48,8 +47,6 @@ export default function Home() {
 
   useEffect(() => {
     getCoins();
-    const intervalId = setInterval(getCoins, 600000);
-    return () => clearInterval(intervalId);
   }, []);
   
   return (
